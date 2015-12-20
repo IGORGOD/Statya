6a81e55f901dc18e3b55ef63ed1d9ff5f31b821e
Ihor I Great <kittoryu@gmail.com>
2015-12-21 01:27:55 +0200

    commit

--- src/ua/ii/db/DBAccessor.java
+++ src/ua/ii/db/DBAccessor.java
@@ -1,11 +1,13 @@
 package ua.ii.db;
 
+import java.beans.PropertyDescriptor;
 import java.io.File;
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import configuration.ConfigLoader;
@@ -95,14 +97,43 @@ public class DBAccessor implements DBCRUDSInterface{
 
 	@Override
 	public <T> void update(T instance) {
-		// TODO Auto-generated method stub
-		
+		try{
+			Field[] fields = instance.getClass().getDeclaredFields();
+			StringBuilder sql=new StringBuilder();
+			sql.append("UPDATE ");
+			sql.append(DAOAnnotationUtils.getStorageName(instance.getClass())+" "+"SET ");
+			for(Field field:fields){
+				sql.append(field.getAnnotation(Stored.class).name()+"=");
+				sql.append("\"" +DAOAnnotationUtils.getFieldValue(instance,field) +"\"");
+				sql.append(", ");
+			}
+			sql=new StringBuilder(sql.substring(0, sql.length()-2));
+			sql.append(" WHERE id");
+			sql.append(DAOAnnotationUtils.getStorageName(instance.getClass()));
+			sql.append("=");
+			sql.append(DAOAnnotationUtils.getFieldValue(instance,fields[0]));
+			System.out.println(sql.toString());
+			statement.execute(sql.toString());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public <T> List<T> select(String SQLString, Class<T> entityClass) {
-		// TODO Auto-generated method stub
-		return null;
+		try{
+			resultSet = statement.executeQuery(SQLString);
+			ArrayList<T> result=new ArrayList<T>();
+
+			while(resultSet.next()){  
+				T instance=DAOAnnotationUtils.fromResultSet(resultSet, entityClass);
+				result.add(instance);   
+			}
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	@Override

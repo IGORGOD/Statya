402534005d6e1413691308353e11de0b039cd9ba
Ihor I Great <kittoryu@gmail.com>
2015-12-21 02:41:35 +0200

    Bus update

--- src/model/Bus.java
+++ src/model/Bus.java
@@ -17,4 +17,40 @@ public class Bus {
 	
 	@Stored(name = "Price", converter = DoubleConverter.class)
 	private double price;
+	
+	public Bus(int idBus, String name, int places, double price){
+		this.idBus = idBus;
+		this.name = name;
+		this.places = places;
+		this.price = price;
+	}
+	
+	public int getIdBus(){
+		return idBus;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getPlaces(){
+		return places;
+	}
+	
+	public double getPrice(){
+		return price;
+	}
+	
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("idBus:");
+		sb.append(idBus);
+		sb.append(" name:");
+		sb.append(name);
+		sb.append(" places:");
+		sb.append(places);
+		sb.append(" price:");
+		sb.append(price);
+		return sb.toString();
+	}
 }
